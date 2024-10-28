e4e76bb39e4e4d1474cd6bfbcb31a1d771b5db37
zaytsev.vi <severniy779@gmail.com>
2024-10-28 19:31:26 +0300

    reasy-state-1.0.54

--- src/store/hooks/use-event.hook.ts
+++ src/store/hooks/use-event.hook.ts
@@ -1,18 +1,19 @@
+/* eslint-disable no-restricted-globals */
 import React from "react";
 
 export const useEvent = <T>(
     type: any,
     onChange: (values: T) => void,
     onChangeType: () => void,
-    onTargetEvent = (ev: Event) => onChange((ev as CustomEvent<T>).detail),
-    doc = document,
+    onTargetEvent = (ev: Event) =>
+        onChange((ev as CustomEvent<{ p: T }>).detail.p),
 ) =>
     React.useLayoutEffect(
         () =>
             type &&
             (onChangeType(),
-            doc.addEventListener(type, onTargetEvent),
-            () => doc.removeEventListener(type, onTargetEvent)),
+            addEventListener(type, onTargetEvent),
+            () => removeEventListener(type, onTargetEvent)),
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [type],
     );
